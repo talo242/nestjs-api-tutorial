5b30b5e5e50a1d2b21f1951eeb1ad503bb2266f2
Ricardo Velasco <talo242@gmail.com>
2023-04-01 00:19:50 -0600

    refactor(auth-service): add jwt strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,15 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKeyProvider: config.get('JWT_SECRET'),
+    });
+  }
+}
