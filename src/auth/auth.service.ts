a4afb44643287d73b29946258ded7f1b1ba7db72
Ricardo Velasco <talo242@gmail.com>
2023-03-31 19:34:36 -0600

    feat(auth-service): add sign up logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return { msg: 'Sign up service' };
+  async signup(dto: AuthDto) {
+    // Hash the password
+    const hash = await argon.hash(dto.password);
+
+    // Create the user
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    const { hash: _, ...userWithoutHash } = user;
+
+    return userWithoutHash;
   }
 
   signin() {
