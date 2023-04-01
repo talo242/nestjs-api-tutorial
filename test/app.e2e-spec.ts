9cbaf70b6535fe7ab8794f872f9412249948a870
Ricardo Velasco <talo242@gmail.com>
2023-04-01 09:21:17 -0600

    test(auth-service): add e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,130 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { like } from 'pactum-matchers';
+import * as pactum from 'pactum';
 
-describe('AppController (e2e)', () => {
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AppModule } from '../src/app.module';
+import { AuthDto } from '../src/auth/dto';
+
+describe('App e2e', () => {
   let app: INestApplication;
+  let prismaService: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      })
+    );
+
     await app.init();
+    await app.listen(3333);
+
+    prismaService = app.get(PrismaService);
+
+    await prismaService.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
+  });
+
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'test@testerson.com',
+      password: 'password',
+    };
+
+    describe('Sign up', () => {
+      it('should create a new user', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+
+      it('should throw an error if email is empty', () => {
+        const { email: _, ...dtoWithoutEmail } = dto;
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dtoWithoutEmail)
+          .expectStatus(400);
+      });
+
+      it('should throw an error if password is empty', () => {
+        const { password: _, ...dtoWithoutPassword } = dto;
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dtoWithoutPassword)
+          .expectStatus(400);
+      });
+
+      it('should throw an error if no body provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({})
+          .expectStatus(400);
+      });
+    });
+
+    describe('Sign in', () => {
+      it('should sign in users', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200);
+      });
+
+      it('should return a token', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .expectJsonMatch({
+            access_token: like(''),
+          });
+      });
+
+      it('should throw an error if email is empty', () => {
+        const { email: _, ...dtoWithoutEmail } = dto;
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dtoWithoutEmail)
+          .expectStatus(400);
+      });
+
+      it('should throw an error if password is empty', () => {
+        const { password: _, ...dtoWithoutPassword } = dto;
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dtoWithoutPassword)
+          .expectStatus(400);
+      });
+
+      it('should throw an error if no body provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({})
+          .expectStatus(400);
+      });
+    });
   });
 });
