3f9f66bee56f46e4c5db1329482efc1581601cf3
Ricardo Velasco <talo242@gmail.com>
2023-03-31 19:41:34 -0600

    refactor(auth-service): handle sign up errors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { Prisma } from '@prisma/client';
 
 @Injectable({})
 export class AuthService {
@@ -12,16 +13,26 @@ export class AuthService {
     const hash = await argon.hash(dto.password);
 
     // Create the user
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    const { hash: _, ...userWithoutHash } = user;
+      const { hash: _, ...userWithoutHash } = user;
 
-    return userWithoutHash;
+      return userWithoutHash;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already in use');
+        }
+
+        throw error;
+      }
+    }
   }
 
   signin() {
